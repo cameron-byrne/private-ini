1dd5e899f42f8274e60a773c80d8a4a977e65aae
Cameron Byrne <cib10@case.edu>
2022-05-13 17:20:56 -0400

    more readable now
    :ok_hand:

--- Learning-multiprocessing.py
+++ Learning-multiprocessing.py
@@ -3,40 +3,51 @@ import time
 import psutil
 
 def main():
+    # change this to change the number of cores being used
+    num_cores = 60
+
     timer = Timer()
     timer.start()
-    for i in range(60):
+
+    # complete a task of adding a ton of numbers together
+    for i in range(num_cores):
         add_nums()
+    print("single-core processing")
     timer.stop()
 
     timer.start()
     q = mp.Queue()
     processes = []
-    for core in range(60):
 
+    # complete the same task but split across several processes
+    for core in range(num_cores):
         p = mp.Process(target=add_nums_multi, args=(q,))
         p.start()
         processes.append(p)
-    for k in range(60):
+
+    # this blocks the program until each process completes
+    for k in range(num_cores):
         processes[k].join()
 
     print(q.qsize())
+    print(f"{num_cores} core processing")
     timer.stop()
 
 
 def add_nums():
     sum = 0
-    for k in range(100000000):  # ten million
+    for k in range(10000000):  # ten million
         sum += k
     return sum
 
 def add_nums_multi(q):
     sum = 0
-    for k in range(100000000):  # ten million
+    for k in range(10000000):  # ten million
         sum += k
     q.put(sum)
 
 
+# not my code, this is just to time things
 class TimerError(Exception):
     """A custom exception used to report errors in use of Timer class"""
 
